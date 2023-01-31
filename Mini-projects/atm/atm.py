03df3c36fd215fd1fe86f97599c77a661a77e3bf
root <montiback22@gmail.com>
2023-01-31 23:03:00 +0100

    version 2

--- Mini-projects/atm/atm.py
+++ Mini-projects/atm/atm.py
@@ -1,36 +1,44 @@
 import re
+import os
+import time
+
+class Invalid_User_Action(Exception):
+	pass
 
 
+def clearing():
+	return os.system('clear')
 
 acc_balance = 0
-pattern = r"([DW]|[dw])\s?([0-9]+)"
-
-for i in range(2):
-	user = input('')
+pattern = r"([DW])\s?([0-9]+)"
+stop_atm = False
+while stop_atm == False:
+	
+	user = input('D - Deposit\nW - Withdraw\nHow much:').upper()
 	test = re.findall(pattern, user)
-	if (test[0][0] == 'D') or (test[0][0] == 'd'):
-		acc_balance +=int(test[0][1])
-	elif (test[0][0] == 'W') or (test[0][0] == 'w'):
-		acc_balance -=int(test[0][1])
-	else:
-		print('Invalid action')
+	clearing()
+	try:	
+		if len(test) == 0:
+			raise IndexError
 
+		elif (test[0][0] != 'D') and (test[0][0] != 'W'):
+			raise Invalid_User_Action
 
-print(acc_balance)
+		else:
+			if (test[0][0] == 'D'):
+				acc_balance +=int(test[0][1])
 
+			elif (test[0][0] == 'W'):
+				acc_balance -=int(test[0][1])
 
+	except Invalid_User_Action:
+			print('test')
+	except IndexError:
+		print('Insert action/amount of cash')
 
+	print(f'Account balance:{acc_balance}')
+	time.sleep(1)
+	stop_atm = bool(int(input('0 - To continue\n1 - To exit\nInsert Number:')))
+	clearing()
 
-
-
-
-
-
-
-
-
-
-# stan konta
-#caly program loop/while
-#cyfry jako regex 
-#D - Deposit, W - Withdraw
+	
